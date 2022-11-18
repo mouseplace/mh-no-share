7a438a8d0c33243de7575ecdbe3a4d1063038296
Brad Parbs <brad@bradparbs.com>
2022-11-18 10:50:36 -0600

    update to cover more

--- mh-no-share.user.js
+++ mh-no-share.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         🐭️ MouseHunt - No Share Buttons
-// @version      1.0.2
+// @version      1.0.3
 // @description  Remove the "Share" buttons that prompt you to share on Facebook.
 // @license      MIT
 // @author       bradp
@@ -18,16 +18,28 @@
 	.journalactions a[data-share-type="journal"],
 	.journalactions a[data-type="journal"],
 	*[src="https://www.mousehuntgame.com//images/ui/buttons/share_green.gif"],
+	*[src="https://www.mousehuntgame.com/images/promo/fbf_live.jpg"],
+	.pageSidebarView,
+	.mousehuntHeaderView-gameBanner,
+	.pageFrameView-footer,
+	.mousehuntFooter,
 	.socialLink,
 	.canShare,
 	.pageSidebarView .fb-page,
 	.canShare .larryTip,
 	.communityGroupView-ad {
+	.mousehuntHeaderView-newsTicker,
+	.actionportfolio,
+	#jsDialog-publishToOwnWall {
 		display: none;
 	}
 
 	#OnboardArrow.onboardPopup.canShare .closeButton {
 		left: 0;
+	}
+
+	.pageFrameView {
+		grid-template-columns: [first] auto [content-start] 760px [content-end] auto [last];
 	}`;
 	document.head.appendChild(addStyles);
 })());
