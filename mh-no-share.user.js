700cc4efb30977b859eacdaf8f8aa4dfa380be4a
Brad Parbs <brad@bradparbs.com>
2023-03-31 19:44:09 -0500

    Update

--- mh-no-share.user.js
+++ mh-no-share.user.js
@@ -6,33 +6,34 @@
 // @author       bradp
 // @namespace    bradp
 // @match        https://www.mousehuntgame.com/*
-// @icon         https://brrad.com/mouse.png
+// @icon         https://i.mouse.rip/mouse.png
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
 ((function () {
-	'use strict';
+  'use strict';
 
-	const addStyles = document.createElement('style');
-	addStyles.innerHTML = `.actionportfolio,
-	.canShare .larryTip,
-	.canShare,
-	.journalactions a[data-share-type="journal"],
-	.journalactions a[data-type="journal"],
-	.pageSidebarView .fb-page,
-	.socialLink,
-	*[src="https://www.mousehuntgame.com//images/ui/buttons/share_green.gif"],
-	#jsDialog-publishToOwnWall,
-	.socialBallots {
-		display: none;
-	}
+  const addStyles = document.createElement('style');
+  addStyles.innerHTML = `.actionportfolio,
+  .canShare .larryTip,
+  .canShare,
+  .journalactions a[data-share-type="journal"],
+  .journalactions a[data-type="journal"],
+  .pageSidebarView .fb-page,
+  .socialLink,
+  *[src="https://www.mousehuntgame.com//images/ui/buttons/share_green.gif"],
+  #jsDialog-publishToOwnWall,
+  .socialBallots {
+    display: none;
+  }
 
-	#OnboardArrow.onboardPopup.canShare .closeButton {
-		left: 0;
-	}
+  #OnboardArrow.onboardPopup.canShare .closeButton {
+    left: 0;
+  }
 
-	.pageFrameView {
-		grid-template-columns: [first] auto [content-start] 760px [content-end] auto [last];
-	}`;
-	document.head.appendChild(addStyles);
+  .pageFrameView {
+    -ms-grid-columns: [first] auto [content-start] 760px [content-end] auto [last];
+    grid-template-columns: [first] auto [content-start] 760px [content-end] auto [last];
+  }`;
+  document.head.appendChild(addStyles);
 })());
